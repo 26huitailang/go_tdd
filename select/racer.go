5668dab1e29c0963b54f301ec73184ced0511f2f
26huitailang <26huitailang@gmail.com>
2019-07-05 17:05:52 +0800

    select timeout and mock the timeout with configurable timeout

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,31 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(u1, u2 string) (winner string) {
-	aDuration := measureResponseTime(u1)
-	bDuration := measureResponseTime(u2)
+var tenSecondTimeout = 10 * time.Second
 
-	if aDuration < bDuration {
-		return u1
-	}
-	return u2
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
-
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
-
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
