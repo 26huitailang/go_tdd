b404f7fe99ab19b72fce5b6ddc7f006d6ca5ab29
26huitailang <26huitailang@gmail.com>
2019-07-06 15:47:38 +0800

    integration test, 注意金字塔原则

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -74,6 +74,20 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
 func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
