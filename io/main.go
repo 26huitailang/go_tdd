bf352b45e385c7828ba80dbaeb663d0f05d6c3df
26huitailang <26huitailang@gmail.com>
2019-07-08 22:17:16 +0800

    integration test for file system store

--- io/main.go
+++ io/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
+const dbFilename = "game.db.json"
 
-	server := NewPlayerServer(NewInMemoryPlayerStore())
+func main() {
+	db, err := os.OpenFile(dbFilename, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFilename, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
