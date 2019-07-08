c0e46d5333fb86c2a2f457d697c293da99653cff
26huitailang <26huitailang@gmail.com>
2019-07-08 23:31:06 +0800

    json.Encoder 作为datbase，不用每次创建

--- io/FileSystemStore.go
+++ io/FileSystemStore.go
@@ -6,15 +6,15 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database *tape
+	database *json.Encoder
 	league   League
 }
 
-func NewFileSystemPlayerStore(database *os.File) *FileSystemPlayerStore {
-	database.Seek(0, 0)
-	league, _ := NewLeague(database)
+func NewFileSystemPlayerStore(file *os.File) *FileSystemPlayerStore {
+	file.Seek(0, 0)
+	league, _ := NewLeague(file)
 	return &FileSystemPlayerStore{
-		database: &tape{database},
+		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}
 }
@@ -41,5 +41,5 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	json.NewEncoder(f.database).Encode(f.league)
+	f.database.Encode(f.league)
 }
