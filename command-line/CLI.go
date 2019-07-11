1779f68667a0395c5890036418b3f86506b70691
26huitailang <26huitailang@gmail.com>
2019-07-11 22:36:51 +0800

    cli

---
+++ command-line/CLI.go
@@ -0,0 +1,49 @@
+package poker
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type CLI struct {
+	playerStore PlayerStore
+	in          *bufio.Scanner
+}
+
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+	}
+}
+func (cli *CLI) PlayPoker() {
+	userInput := cli.readLine()
+	cli.playerStore.RecordWin(extracctWinner(userInput))
+}
+
+func extracctWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
+}
+
+func (cli *CLI) readLine() string {
+	cli.in.Scan()
+	return cli.in.Text()
+}
+
+func FileSystemPlayerStoreFromFile(path string) (*FileSystemPlayerStore, error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("problem opening %s %v", path, err)
+	}
+
+	store, err := NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem creating file system player store, %v", err)
+	}
+
+	return store, nil
+}
