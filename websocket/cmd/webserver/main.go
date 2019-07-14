abef14401b2d6bb66fde49c81b005b483a818d07
26huitailang <26huitailang@gmail.com>
2019-07-14 14:58:55 +0800

    200 /game test

---
+++ websocket/cmd/webserver/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	poker "github.com/26huitailang/go_tdd/websocket"
+)
+
+const dbFilename = "game.db.json"
+
+func main() {
+	store, _, err := poker.FileSystemPlayerStoreFromFile(dbFilename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := poker.NewPlayerServer(store)
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
