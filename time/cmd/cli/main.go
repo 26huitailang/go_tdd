fc267bdf9d638b6778b2bd1d2d3f22f7c0ec6415
26huitailang <26huitailang@gmail.com>
2019-07-14 14:49:35 +0800

    3 return

--- time/cmd/cli/main.go
+++ time/cmd/cli/main.go
@@ -11,14 +11,17 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	store, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer close()
+
+	game := poker.NewTexasHoldem(poker.BlindAlerterFunc(poker.StdOutAlerter), store)
+	cli := poker.NewCLI(os.Stdin, os.Stdout, game)
 
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-
-	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
+	cli.PlayPoker()
 }
